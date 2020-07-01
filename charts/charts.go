72bc4a34084924f268b60de606c7a8e7dbca1f4a
Nikolay <nikolay.dubina.pub@gmail.com>
2020-07-02 03:12:34 +0800

    cleanup

--- charts/charts.go
+++ charts/charts.go
@@ -21,7 +21,6 @@ var weekdaysPos = map[time.Weekday]int{
 
 const (
 	numWeeksYear = 52
-	numDaysYear  = 366              // always account for leap day
 	numWeekCols  = numWeeksYear + 1 // 53 * 7 = 371 > 366
 	margin       = 7                // should be odd number for best result if using month separator
 	boxSize      = 25
@@ -49,17 +48,16 @@ func MakeYearDayHeatmapHoriz(year int, countByDay map[int]int, colorScale colors
 	yearStartDate := time.Date(year, 1, 1, 1, 1, 1, 1, time.UTC)
 	vIdx := weekdaysPos[yearStartDate.Weekday()]
 
-	// start from 1 since time.DayOfYear is expected to return from 1 ~ 366
-	for currDay := yearStartDate; currDay.Year() == year; currDay = currDay.Add(time.Hour * 24) {
+	for day := yearStartDate; day.Year() == year; day = day.Add(time.Hour * 24) {
 		y = (boxSize + margin) * vIdx
 
 		r := image.Rect(x, y, x+boxSize, y+boxSize)
-		val := float64(countByDay[currDay.YearDay()]) / float64(maxCount)
+		val := float64(countByDay[day.YearDay()]) / float64(maxCount)
 		color := colorScale.GetColor(val)
 		draw.Draw(img, r, &image.Uniform{color}, image.ZP, draw.Src)
 
 		if drawMonthSeparator {
-			if currDay.Day() == 1 && currDay.Month() != time.January {
+			if day.Day() == 1 && day.Month() != time.January {
 				marginSep := margin / 2
 
 				closeLeft := image.Point{X: x - marginSep - 1, Y: y - marginSep - 1}
@@ -85,20 +83,6 @@ func MakeYearDayHeatmapHoriz(year int, countByDay map[int]int, colorScale colors
 	return img
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func max(a, b int) int {
-	if a < b {
-		return b
-	}
-	return a
-}
-
 // drawLineAxis draws line parallel to X or Y axis
 func drawLineAxis(img draw.Image, a image.Point, b image.Point, col color.Color) {
 	switch {
@@ -107,12 +91,20 @@ func drawLineAxis(img draw.Image, a image.Point, b image.Point, col color.Color)
 		return
 	// vertical
 	case a.X == b.X:
-		for q := min(a.Y, b.Y); q <= max(a.Y, b.Y); q++ {
+		y1, y2 := a.Y, b.Y
+		if y1 > y2 {
+			y1, y2 = y2, y1
+		}
+		for q := y1; q <= y2; q++ {
 			img.Set(a.X, q, col)
 		}
 	// horizontal
 	case a.Y == b.Y:
-		for q := min(a.X, b.X); q <= max(a.X, b.X); q++ {
+		x1, x2 := a.X, b.X
+		if x1 > x2 {
+			x1, x2 = x2, x1
+		}
+		for q := x1; q <= x2; q++ {
 			img.Set(q, a.Y, col)
 		}
 	default:
